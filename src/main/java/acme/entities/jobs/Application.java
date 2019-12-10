2e35c3b358b9de23a35aa0d109ae5c95f37770cc
gonguelun <gonguelun@alum.us.es>
2019-12-10 16:07:07 +0100

    Task 014: finished

--- src/main/java/acme/entities/jobs/Application.java
+++ src/main/java/acme/entities/jobs/Application.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -23,6 +25,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "job_id, status asc, moment desc"),
+})
 public class Application extends DomainEntity {
 
 	private static final long	serialVersionUID	= 1L;
